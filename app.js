80d72a588a7d03dd3f91c928f2874be99060b20f
Utkarsh Mishra <utmishra1998@gmail.com>
2023-02-07 21:16:26 +0530

    Data security and data sanatization

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanatize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController')
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const viewRouter = require('./routes/viewRoutes');
@@ -13,20 +18,46 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    window: 60 * 60 * 1000,
+    message: 'Too many requests form this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body 
+app.use(express.json({ limit: '10kb'}));
+
+// Data sanatization against NoSQL query injection
+app.use(mongoSanatize());
+
+// Data sanatization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}));
+
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     // console.log(req.headers);
-
     next();
 });
 
