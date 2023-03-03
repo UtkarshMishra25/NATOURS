100b54d36ac8ea22a7c4f3d2e33971a82c97d8a0
Utkarsh Mishra <utmishra1998@gmail.com>
2023-03-03 15:31:03 +0530

    Preventing Duplicate Reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -34,7 +34,8 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
-        max: [5, 'Rating must be below 5.0']
+        max: [5, 'Rating must be below 5.0'],
+        sat: val => Math.round(val * 10) / 10 // 4.66666, 46.66666, 47, 4.7
     },
     ratingsQuantity: {
         type: Number,
