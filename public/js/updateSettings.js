cf19713b05713a09e4bd8be80123c4c87818484e
Utkarsh Mishra <utmishra1998@gmail.com>
2023-03-22 15:50:08 +0530

    Updating User Password with API

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,21 +2,24 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateDate = async (name, email) => {
-    try {
-        const res = await axios({
-            method: 'PATCH',
-            url: 'http:/127.0.0.1:3000/api/v1/users/updateMe',
-            data: {
-                name,
-                email
-            }
-        });
+// type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
+  try {
+    const url =
+      type === 'password'
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
 
-        if (res.data.status === 'success') {
-            showAlert('success', 'Data updated successfully!');
-        }
-    } catch (err) {
-        showAlert('error', err.response.data.message);
+    const res = await axios({
+      method: 'PATCH',
+      url,
+      data
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
 };
