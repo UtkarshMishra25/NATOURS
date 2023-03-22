7007b582708e0634911abe7dd4033a81f9cc548e
Utkarsh Mishra <utmishra1998@gmail.com>
2023-03-22 12:36:33 +0530

    Updating User Data

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -45,3 +45,19 @@ exports.getAccount = (req, res) => {
     title: 'your account'
   });
 };
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, {
+    name: req.body.name,
+    email: req.body.email
+  },
+  {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).render('account', {
+    title: 'your account',
+    user: updatedUser
+  });
+});
