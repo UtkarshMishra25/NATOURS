4440d8cf8b45245e8ebcccf809b527176344d892
Utkarsh Mishra <utmishra1998@gmail.com>
2023-03-22 21:15:19 +0530

    Image Uploads Using Multer: Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
+
+const upload = multer({ dest: 'public/img/users' });
 
 const router = express.Router();
 
@@ -17,7 +20,7 @@ router.use(authController.protect);
 router.patch('/updateMyPassword', authController.updatePassword);
 
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updaetMe);
+router.patch('/updateMe', upload.single('photo'), userController.updaetMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
